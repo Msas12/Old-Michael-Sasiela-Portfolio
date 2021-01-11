31c706cc88055f5ceee5aedc3a3fe85a3c5ca83a
Mike Sasiela <m5455y@gmail.com>
2021-01-10 18:14:03 -0600

    reformatting portfolio. added color palette

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
-
-
-
-
-
+    
+    // Function to clear Email Form
+    function clearForm() {
+        $('#emailForm').reset()
+        $('.form-control').clear()
+    }
+    
+    
+    
+    // EVENT LISTENERS----------------------------------------------
+    $('.send-email-button').on('click', function() {
+        clearForm()
+    })
 
 
     // Materialize Advent Listeners for functionality
