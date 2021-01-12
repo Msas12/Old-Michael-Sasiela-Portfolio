30bd5b5330b620ca6192de3863fd919b0b070611
Mike Sasiela <m5455y@gmail.com>
2021-01-11 23:41:03 -0600

    fixed email submission form

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
     
-    // Function to clear Email Form
-    function clearForm() {
-        $('#emailForm').reset()
-        $('.form-control').clear()
-    }
     
     
     
-    // EVENT LISTENERS----------------------------------------------
-    $('.send-email-button').on('click', function() {
-        clearForm()
-    })
+    // // EVENT LISTENERS
+    // $('.send-email-button').on('click', function() {
+    //     clearForm()
+    // })
+
   });
